a676405ea382e298ba7dc77895c2808ae6f4b826
williechin <wjcin26@gmail.com>
2024-09-30 09:47:13 +0900

    Update send_email.php
    username

--- send_email.php
+++ send_email.php
@@ -25,7 +25,7 @@ try {
     $mail->isSMTP();                                      // Set mailer to use SMTP
     $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
     $mail->SMTPAuth = true;                               // Enable SMTP authentication
-    $mail->Username = 'leaderteamhr@gmail.com';                 // SMTP username
+    $mail->Username = 'leaderteamhr';                 // SMTP username
     $mail->Password = '트 l x 아 푸 드 c 티 c c 스 디 v c';                           // SMTP password
     $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
     $mail->Port = 465;                                    // TCP port to connect to
