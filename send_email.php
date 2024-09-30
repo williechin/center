62944589d73309b0ad7aab4435adfbc7debe7bba
williechin <wjcin26@gmail.com>
2024-09-30 09:25:34 +0900

    Update send_email.php

--- send_email.php
+++ send_email.php
@@ -26,7 +26,7 @@ try {
     $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
     $mail->SMTPAuth = true;                               // Enable SMTP authentication
     $mail->Username = 'leaderteamhr@gmail.com';                 // SMTP username
-    $mail->Password = '트 l x 아 푸 드 c 티 c c 스 디 v c';                           // SMTP password
+    $mail->Password = 'willie900**';                           // SMTP password
     $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
     $mail->Port = 465;                                    // TCP port to connect to
 
